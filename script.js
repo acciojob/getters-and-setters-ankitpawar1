06b4a2dfa446e9a5b402318a183dbe6fb1025b38
ankitpawar1 <54860507+ankitpawar1@users.noreply.github.com>
2024-08-28 20:52:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
+    
+    get name() {
+        return this._name;
+    }
+    
+    set age(newAge) {
+        this._age = newAge;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
-// Do not change the code below this line
+// The following lines will make the classes global in the window scope
 window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
